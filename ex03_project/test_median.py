edafb7481cc865f76d097ffb67d0e408216ee710
Nicolai Munsterhjelm <nicmunst@nmbu.no>
2019-10-03 13:53:58 +0200

    First three tests of Task B

---
+++ ex03_project/test_median.py
@@ -0,0 +1,60 @@
+
+# -*- coding: utf-8 -*-
+
+__author__ = 'Nicolai Munsterhjelm'
+__email__ = 'nicmunst@nmbu.no'
+
+# The source of the code for the median() function is the
+# INF200 Exercise 3 text by Yngve Mardal Moe.
+
+def median(data):
+    """
+    Returns median of data.
+
+    :param data: An iterable of containing numbers
+    :return: Median of data
+    """
+
+    sdata = sorted(data)
+    n = len(sdata)
+    return (sdata[n//2] if n % 2 == 1
+            else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))
+
+
+def test_single_element():
+    """Tests that the median function returns the correct value for
+    a single element list"""
+    assert median([5]) == 5
+
+
+def test_odd_number():
+    """Tests that the median function returns the correct value for lists
+    with odd numbers of elements"""
+    assert median([5, 4, 3, 2, 1]) == 3
+    assert median([1, 43, 21]) == 21
+
+
+def test_even_number():
+    """Tests that the median function returns the correct value for lists
+     with even number of elements"""
+    assert median([4, 3, 2, 1]) == 2.5
+    assert median([1, 43]) == 22
+
+
+def test_ordered():
+    """Tests that the median function returns the correct value for lists
+    with ordered elements"""
+
+
+def test_reverse_ordered():
+    """Tests that the median function returns the correct value for
+     reverse-ordered lists"""
+
+
+def test_unordered():
+    """Tests that the median function returns the correct value for
+    unordered lists"""
+
+
+def test_empty_list():
+    
--- ex03_project/test_sorting.py
+++ ex03_project/test_sorting.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 __author__ = 'Nicolai Munsterhjelm'
 __email__ = 'nicmunst@nmbu.no'
