2105c1ffd81f82d13f117efbaa6212ef08e6c962
Nicolai Munsterhjelm <nicmunst@nmbu.no>
2019-11-15 09:24:50 +0100

    Auto stash before merge of "pa02" and "origin/pa02"
    Finished class Board

--- src/nicolai_munsterhjelm_ex/pa02/chutes_simulation.py
+++ src/nicolai_munsterhjelm_ex/pa02/chutes_simulation.py
@@ -5,11 +5,27 @@ __email__ = 'alov@nmbu.no', 'nicmunst@nmbu.no'
 
 
 class Board:
-    def __init__(self, *ladders, *chutes, *goal):
-        self.ladders = {1: 40, 8: 10, 36: 52, 43: 62, 49: 79, 65: 82,
-                            68: 85}
-        self.chutes = {24: 5, 33: 3, 42: 30, 56: 37, 64: 27, 74: 12,
-                            87: 70}
+    def __init__(self, ladders=None, chutes=None, goal=90):
+        if ladders is None:
+            ladders = [(1, 40), (8, 10), (36, 52), (43, 62), (49, 79),
+                       (65, 82), (68, 85)]
+        if chutes is None:
+            chutes = [(24, 5), (33, 3), (42, 30), (56, 37), (64, 27),
+                      (74, 12), (87, 70)]
+
+        self.ladders = dict(ladders)
+        self.chutes = dict(chutes)
+        self.goal = goal
+
+    def goal_reached(self, position):
+        return position >= 90
+
+    def position_adjustment(self, position):
+        if position in self.ladders.keys():
+            return self.ladders[position] - position
+        elif position in self.chutes.keys():
+            return self.chutes[position] - position
+        return 0
 
 """
 `Board`` class
@@ -32,7 +48,7 @@ and the goal.
 """
 class Player:
     def __init__(self):
-
+        pass
 """
 ``Player`` class
 ----------------
